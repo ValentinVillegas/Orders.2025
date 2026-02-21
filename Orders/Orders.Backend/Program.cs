efb76a3888f37dbb79838744ec9a92b41982d34e
ValentinVillegas <j.valentin1@hotmail.com>
2026-02-20 18:21:26 -0600

    Add State and City Controller, SeederDB and Json Ignore Cycles

--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -4,12 +4,16 @@ using Orders.Backend.Repositories.Implementations;
 using Orders.Backend.Repositories.Interfaces;
 using Orders.Backend.UnitsOfWork.Implementations;
 using Orders.Backend.UnitsOfWork.Interfaces;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.
+    AddControllers().
+    AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles); //Ignora las redundancias cíclicas
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ Orders/Orders.Shared/Interfaces/IEntityWithName.cs
@@ -0,0 +1,6 @@
+﻿namespace Orders.Shared.Interfaces;
+
+public interface IEntityWithName
+{
+    public string Name { get; set; }
+}
